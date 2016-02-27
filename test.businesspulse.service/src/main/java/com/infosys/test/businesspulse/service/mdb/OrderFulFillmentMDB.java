652c50929492a4026f2cb4746f60a66d2043413a
ArunRajV4444 <mp3varaj@gmail.com>
2016-02-27 15:50:12 +0100

    Corrections in MDB

--- test.businesspulse.service/src/main/java/com/infosys/test/businesspulse/service/mdb/OrderFulFillmentMDB.java
+++ test.businesspulse.service/src/main/java/com/infosys/test/businesspulse/service/mdb/OrderFulFillmentMDB.java
@@ -2,10 +2,17 @@ package com.infosys.test.businesspulse.service.mdb;
 
 import javax.ejb.MessageDriven;
 import javax.ejb.ActivationConfigProperty;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
-@MessageDriven(mappedName = "OrderFulFillmentJMSDest", activationConfig = { @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue") })
+@MessageDriven(mappedName = "OrderFulFillmentJMSDest", 
+name = "OrderFulFillmentMDB",
+activationConfig = { 
+	@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+	@ActivationConfigProperty(propertyName = "connectionFactoryJndiName", propertyValue = "HackathonConnectionFactory"),
+	@ActivationConfigProperty(propertyName = "destinationJndiName", propertyValue = "OrderFulFillmentJMSDest"),
+})
 public class OrderFulFillmentMDB implements MessageListener
 {
     
@@ -15,10 +22,17 @@ public class OrderFulFillmentMDB implements MessageListener
 	try
 	{
 	    System.out.println("Started Order fulfillment processing");
+	    System.out.println(message.getJMSMessageID());
+	    
 	    Thread.sleep(50000);
 	    System.out.println("Completed Order fulfillment processing");
 	}
 	catch (InterruptedException e)
+	{
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+	catch (JMSException e)
 	{
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
