80dd2b01c77508aa4a01e8782ed4699153066f79
ArunRajV4444 <mp3varaj@gmail.com>
2016-02-27 18:21:29 +0100

    Committing Integration between OrderFulfillmentMDB to BillingService EJB

--- test.businesspulse.service/src/main/java/com/infosys/test/businesspulse/service/mdb/OrderFulFillmentMDB.java
+++ test.businesspulse.service/src/main/java/com/infosys/test/businesspulse/service/mdb/OrderFulFillmentMDB.java
@@ -1,18 +1,20 @@
 package com.infosys.test.businesspulse.service.mdb;
 
-import javax.ejb.MessageDriven;
+import java.util.Hashtable;
+
 import javax.ejb.ActivationConfigProperty;
-import javax.jms.JMSException;
+import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.naming.Context;
+import javax.naming.InitialContext;
 
-@MessageDriven(mappedName = "OrderFulFillmentJMSDest", 
-name = "OrderFulFillmentMDB",
-activationConfig = { 
-	@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
-	@ActivationConfigProperty(propertyName = "connectionFactoryJndiName", propertyValue = "HackathonConnectionFactory"),
-	@ActivationConfigProperty(propertyName = "destinationJndiName", propertyValue = "OrderFulFillmentJMSDest"),
-})
+import com.infosys.test.businesspulse.service.ejb.BillingService;
+
+@MessageDriven(mappedName = "OrderFulFillmentJMSDest",
+        name = "OrderFulFillmentMDB",
+        activationConfig = { @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"), @ActivationConfigProperty(propertyName = "connectionFactoryJndiName", propertyValue = "HackathonConnectionFactory"),
+                @ActivationConfigProperty(propertyName = "destinationJndiName", propertyValue = "OrderFulFillmentJMSDest"), })
 public class OrderFulFillmentMDB implements MessageListener
 {
     
@@ -24,15 +26,20 @@ public class OrderFulFillmentMDB implements MessageListener
 	    System.out.println("Started Order fulfillment processing");
 	    System.out.println(message.getJMSMessageID());
 	    
-	    Thread.sleep(50000);
+	    Hashtable<String, String> env = new Hashtable<String, String>();
+	    env.put(Context.INITIAL_CONTEXT_FACTORY, "weblogic.jndi.WLInitialContextFactory");
+	    env.put(Context.PROVIDER_URL, "t3://localhost:7001");
+	    Context ctx = new InitialContext(env);
+	    //Context ctx = new InitialContext();
+	    BillingService billingProvider = (BillingService) ctx.lookup("BillingServiceProvider#com.infosys.test.businesspulse.service.ejb.BillingService");
+	    System.out.println("After Context Lookup of Billing service. Invoking Billing service EJB");
+	    billingProvider.syncBillingInfo(message.getJMSMessageID());
+	    System.out.println("After Billing service EJB");
+	    
+	    Thread.sleep(5000);
 	    System.out.println("Completed Order fulfillment processing");
 	}
-	catch (InterruptedException e)
-	{
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-	catch (JMSException e)
+	catch (Exception e)
 	{
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
